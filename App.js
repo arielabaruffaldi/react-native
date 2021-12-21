8e9869437634a766bd5ee271b51d700d5ec43fd3
arielabaruffaldi <baruffaldiariela@gmail.com>
2021-12-21 20:50:11 -0300

    removing unused components

--- App.js
+++ App.js
@@ -9,49 +9,29 @@
 import React from 'react';
 import type { Node } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
   useColorScheme,
-  View,
 } from 'react-native';
 
 import {
   Colors,
-  Header,
 } from 'react-native/Libraries/NewAppScreen';
 
 
 const App: () => Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={[styles.container]}>
-          <Text
-            style={[
-              styles.sectionTitle,
-              {
-                color: isDarkMode ? Colors.white : Colors.black,
-              },
-            ]}>
-            Hola Coder
-          </Text>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <Text
+      style={[
+        styles.sectionTitle,
+        {
+          color: isDarkMode ? Colors.white : Colors.black,
+        },
+      ]}>
+      Hola Coder
+    </Text>
   );
 };
 
