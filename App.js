ba5c09257633fb30b52dd13b13b7ecf5f37a64f8
arielabaruffaldi <baruffaldiariela@gmail.com>
2022-01-04 22:09:36 -0300

    feat(styles): add styles

--- App.js
+++ App.js
@@ -1,12 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   StyleSheet,
   Text,
@@ -15,40 +7,55 @@ import {
   Button,
   FlatList,
   TouchableOpacity,
-} from "react-native";
+} from 'react-native';
+
 const App = () => {
   const [taskList, setTaskList] = useState([]);
   const [task, setTask] = useState();
+  console.log('HOLAAA');
 
   const addTast = () => {
     setTaskList([...taskList, { id: Math.random(), task }]);
+    setTask('');
   };
 
   const deleteTask = (id) => {
-    console.warn("id to delete", id);
     setTaskList(taskList.filter((task) => task.id !== id));
   };
-
+  const renderItem = ({ item }) => {
+    return (
+      <View style={styles.taskList} key={item.id}>
+        <Text style={styles.taskListItem}>{item.task}</Text>
+        <TouchableOpacity
+          style={styles.buttonDelete}
+          onPress={() => deleteTask(item.id)}
+        >
+          <Text style={{ color: '#fff' }}>x</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
   return (
     <View style={[styles.container]}>
-      <Text style={[styles.sectionTitle]}>Hola Coder</Text>
       <TextInput
+        defaultValue={task}
         onChangeText={(text) => setTask(text)}
         placeholder="Name a task"
+        style={styles.input}
       />
-      <Button onPress={addTast} title="Save" />
-      <View>
+      <TouchableOpacity
+        disabled={!task}
+        style={styles.buttonSave}
+        onPress={addTast}
+      >
+        <Text style={styles.buttonSaveText}>Save</Text>
+      </TouchableOpacity>
+      <View style={styles.taskListContainer}>
         <Text style={styles.taskListTitle}>Task List</Text>
         <FlatList
           data={taskList}
-          renderItem={({ item, index }) => (
-            <View key={index}>
-              <Text>{item.task}</Text>
-              <TouchableOpacity onPress={() => deleteTask(item.id)}>
-                <Text>x</Text>
-              </TouchableOpacity>
-            </View>
-          )}
+          keyExtractor={(item) => item.id}
+          renderItem={renderItem}
         />
       </View>
     </View>
@@ -57,24 +64,65 @@ const App = () => {
 
 const styles = StyleSheet.create({
   container: {
-    display: "flex",
-    alignContent: "center",
-    justifyContent: "center",
+    display: 'flex',
+    alignContent: 'center',
+    justifyContent: 'center',
     margin: 24,
   },
-  sectionTitle: {
-    fontSize: 34,
-    margin: 24,
-    fontWeight: "600",
+  input: {
+    borderWidth: 1,
+    marginBottom: 24,
+    borderColor: '#ddd',
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    color: "#333"
+  },
+  buttonSave: {
+    backgroundColor: '#333',
+    padding: 20,
+    color: "white",
+  },
+  buttonSaveText: {
+    color: "white",
+    fontSize: 14,
+    textTransform: "uppercase",
     textAlign: "center",
+    fontWeight: "bold",
   },
   taskListTitle: {
     fontSize: 20,
-    marginTop: 24,
+    marginTop: 48,
     marginBottom: 24,
-    fontWeight: "600",
-    color: "#000",
-  }
+    fontWeight: '600',
+    color: '#333',
+  },
+  taskList: {
+    display: "flex",
+    flexDirection: "row",
+    backgroundColor: "#ffc926",
+    padding: 20,
+    marginVertical: 8,
+    color: "#fff",
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  taskListItem: {
+    fontSize: 18,
+    color: "#fff",
+    marginRight: 20,
+  },
+  buttonDelete: {
+    color: "#fff",
+    borderWidth: 1,
+    borderRadius: 100,
+    width: 24,
+    height: 24,
+    borderColor: "white",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignContent: "center",
+  },
 });
 
 export default App;
